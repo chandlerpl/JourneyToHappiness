cf7f539b2e16d00233503c7eeebca7b26577ab88
TheMightyChloroplast <isabela.b.78@gmail.com>
2022-08-21 14:59:11 +0100

    .
    .

--- Assets/Scripts/UIUpdater.cs
+++ Assets/Scripts/UIUpdater.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIUpdater : MonoBehaviour
 {
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI happinessText;
+    public TextMeshProUGUI comfortText;
+    public TextMeshProUGUI day;
     private void Start()
     {
-        GameManager.ComfortChanged += comfort => { Debug.Log("" + comfort); };
-        GameManager.HappinessChanged += happiness => { Debug.Log("" + happiness); };
-        GameManager.BalanceChanged += balance => { Debug.Log("" + balance); };
-        GameManager.CurrentDayChanged += newDay => { Debug.Log("" + newDay); };
+        GameManager.ComfortChanged += comfort => { comfortText.text = comfort.ToString(); };
+        GameManager.HappinessChanged += happiness => { happinessText.text = happiness.ToString(); };
+        GameManager.BalanceChanged += balance => { moneyText.text = balance.ToString(); };
+        GameManager.CurrentDayChanged += newDay => { day.text = newDay.ToString(); };
+        
+        
     }
 
+    
 }
